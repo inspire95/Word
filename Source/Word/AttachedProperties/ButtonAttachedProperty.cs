9e4a0a2a545153c09bd139f116ce84ebe26508ca
inspire95 <coneser2016@gmail.com>
2020-03-15 21:44:38 +0100

    Login Page ViewModel Binding

---
+++ Source/Word/AttachedProperties/ButtonAttachedProperty.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Word
+{
+    /// <summary>
+    /// The IsBusy attached property for a anything that wants to flag if the control is busy
+    /// </summary>
+    public class IsBusyProperty : BaseAttachedProperty<IsBusyProperty, bool>
+    {
+    }
+}
--- Source/Word/Pages/BasePage.cs
+++ Source/Word/Pages/BasePage.cs
@@ -9,8 +9,18 @@ namespace Word
     /// <summary>
     /// A base page for all pages to gain base functionality
     /// </summary>
-    public class BasePage : Page
+    public class BasePage<VM> : Page
+        where VM : BaseViewModel, new()
     {
+        #region Private Member
+
+        /// <summary>
+        /// The View Model associated with this page
+        /// </summary>
+        private VM mViewModel;
+
+        #endregion
+
         #region Public Properties
 
         /// <summary>
@@ -27,7 +37,28 @@ namespace Word
         /// The time any slide animation takes to complete
         /// </summary>
         public float SlideSeconds { get; set; } = 0.8f;
+            
+        /// <summary>
+        /// The View Model associated with this page
+        /// </summary>
+        public VM ViewModel
+        {
+            get { return mViewModel; }
+            set
+            {
+                // If nothing has changed, return
+                if (mViewModel == value)
+                    return;
 
+                // Update the value
+                mViewModel = value;
+
+                // Set the data context for this page
+                this.DataContext = mViewModel;
+            }
+        }
+
+            
         #endregion
 
         #region Constructor
@@ -43,6 +74,9 @@ namespace Word
 
             // Listen out for the page loading
             this.Loaded += BasePage_Loaded;
+            
+            // Create a default view model
+            this.ViewModel = new VM();
         }
 
         #endregion
