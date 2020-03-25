c5cca8bb5d43e452df22e11277c90ef355659870
inspire95 <coneser2016@gmail.com>
2020-03-25 09:20:47 +0100

    Start making chat bubbles

---
+++ Source/Word/Controls/Chat/ChatMessage/ChatMessageListControl.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace Word
+{
+    /// <summary>
+    /// Interaction logic for ChatMessageListControl.xaml
+    /// </summary>
+    public partial class ChatMessageListControl : UserControl
+    {
+        public ChatMessageListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Source/Word/Controls/Chat/ChatMessage/ChatMessageListItemControl.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace Word
+{
+    /// <summary>
+    /// Interaction logic for ChatMessageListItemControl.xaml
+    /// </summary>
+    public partial class ChatMessageListItemControl : UserControl
+    {
+        public ChatMessageListItemControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
