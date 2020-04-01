9f039bdbc0ed305bceb717cda0c3767cb2ba3cb0
inspire95 <coneser2016@gmail.com>
2020-04-01 13:56:39 +0200

    Send message chat bubble styling

---
+++ Source/Word.Core/ViewModels/Chat/ChatMessage/ChatMessageListViewModel.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Word.Core
+{
+    /// <summary>
+    /// A view model for a chat message thread list
+    /// </summary>
+    public class ChatMessageListViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The chat thread items for the list
+        /// </summary>
+        public List<ChatMessageListItemViewModel> Items { get; set; }
+    }
+}
